f357e9119d356345b063a7d71a34cc7014c3a3e2
jeanramalho <jeanramalho.dev@gmail.com>
2023-03-17 09:12:14 -0300

    Melhora Insere Botão

--- components/welcome/index.jsx
+++ components/welcome/index.jsx
@@ -1,10 +1,12 @@
 import Styles from "./welcome.module.scss"
+import Button from "../button"
 
 const Welcome = () => {
     return <div className={Styles.container}>
         <div className={Styles.text}>
             <h1>Melhor agência de marketing do bairro</h1>
             <p>Somos uma agência de performance digital, aceleramos vendas e aquisição de leads para grandes marcas.</p>
+            <Button title="Aumentar Vendas"/>
         </div>
         <div className={Styles.image}></div>
     </div>
