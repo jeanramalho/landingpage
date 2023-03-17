c9121b003114d3151b4fca8fd078249376e7f4d9
jeanramalho <jeanramalho.dev@gmail.com>
2023-03-17 08:58:18 -0300

    Cria divs das sessões do componente welcome

--- components/welcome/index.jsx
+++ components/welcome/index.jsx
@@ -2,8 +2,8 @@ import Styles from "./welcome.module.scss"
 
 const Welcome = () => {
     return <div className={Styles.container}>
-
-        
+        <div></div>
+        <div></div>
     </div>
 }
 
