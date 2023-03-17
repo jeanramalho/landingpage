b8a7e0cde1391698eac868d7793319d04cd308fb
jeanramalho <jeanramalho.dev@gmail.com>
2023-03-17 09:06:39 -0300

    Insere a sessão welcome na home

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,12 @@
 import styles from './Home.module.scss'
 import Header from "../components/header"
+import Welcome from "../components/welcome"
 
 export default function Home() {
   return (
-    <>
+    <div className={Styles.container}>
       <Header />
-    </>
+      <Welcome />
+    <div/>
   )
 }
