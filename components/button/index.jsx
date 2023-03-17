d74c68af5ac6944eb2813683bdfc53cf7dd879bd
jeanramalho <jeanramalho.dev@gmail.com>
2023-03-17 09:16:10 -0300

    Criando validação para mudança de botão

--- components/button/index.jsx
+++ components/button/index.jsx
@@ -1,6 +1,12 @@
 import Styles from "./button.module.scss"
 
-const Button = ({title}) => {
+const Button = ({title, kind}) => {
+    const generationClassKing =  () => {
+        if(kind === "secondary") {
+            return "secondary"
+        }
+        return "primary"
+    }
     return <button className={Styles.button}>{title}</button>
 }
 
