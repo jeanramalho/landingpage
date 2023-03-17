21af5294edf6199a09d96dbb2186fbc2edf01cd4
jeanramalho <jeanramalho.dev@gmail.com>
2023-03-17 08:57:08 -0300

    Começa a estruturar componente welcome

--- components/welcome/index.jsx
+++ components/welcome/index.jsx
@@ -1,5 +1,10 @@
-const Welcome = () => {
+import Styles from "./welcome.module.scss"
 
+const Welcome = () => {
+    return <div className={Styles.container}>
+
+        
+    </div>
 }
 
 export default Welcome
