9af98c3cc07bd107cda4e8d71c6c938ae210b0ba
jeanramalho <jeanramalho.dev@gmail.com>
2023-03-17 09:09:40 -0300

    Insere welcome na home

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,10 +3,9 @@ import Header from "../components/header"
 import Welcome from "../components/welcome"
 
 export default function Home() {
-  return (
-    <div className={Styles.container}>
+  return <div className={styles.container}>
       <Header />
       <Welcome />
-    <div/>
-  )
+    </div>
+  
 }
