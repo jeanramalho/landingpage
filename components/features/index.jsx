820802d89688189060ee63dab373a2560d4aefd8
jeanramalho <jeanramalho.dev@gmail.com>
2023-03-21 16:26:43 -0300

    Ajusta nome da classe

--- components/features/index.jsx
+++ components/features/index.jsx
@@ -4,7 +4,7 @@ const Features = () => {
         <h1>Neste bairro, não há agência melhor. Garantimos.</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
 
-        <div className="Styles.containerFeatures">
+        <div className={Styles.containerFeatures}>
 
         </div>
 
