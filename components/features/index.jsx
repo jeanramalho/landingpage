0092986a0605feb33e7ae5ff82fa30bd1742d862
jeanramalho <jeanramalho.dev@gmail.com>
2023-03-21 16:24:40 -0300

    insere paragrafo

--- components/features/index.jsx
+++ components/features/index.jsx
@@ -2,7 +2,8 @@
 const Features = () => {
     return <div>
         <h1>Neste bairro, não há agência melhor. Garantimos.</h1>
-        
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+
     </div>
 }
 
