62ebc218610ace79454d7b269012b08b5095630d
jeanramalho <jeanramalho.dev@gmail.com>
2023-03-21 16:26:09 -0300

    Começa a cria blocos

--- components/features/index.jsx
+++ components/features/index.jsx
@@ -4,6 +4,10 @@ const Features = () => {
         <h1>Neste bairro, não há agência melhor. Garantimos.</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
 
+        <div className="Styles.containerFeatures">
+
+        </div>
+
     </div>
 }
 
