342ed187c34d80c2bc3bf87ee8a91ad36040f5ab
jeanramalho <jeanramalho.dev@gmail.com>
2023-03-22 08:46:17 -0300

    Começa a criar blocos de visualização na sessão features

--- components/features/index.jsx
+++ components/features/index.jsx
@@ -5,7 +5,11 @@ const Features = () => {
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
 
         <div className={Styles.features}>
-            <div className={Styles.feature}></div>
+            <div className={Styles.feature}>
+                icon
+                <h1></h1>
+                <p></p>
+            </div>
             <div className={Styles.feature}></div>
             <div className={Styles.feature}></div>
             <div className={Styles.feature}></div>
